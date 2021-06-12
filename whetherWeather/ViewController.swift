7f5921eea0cf8a33b97a32c3a60047286a875359
Brett Bauman <bauman.brett3@gmail.com>
2021-06-11 18:34:27 -0700

    initial comm

---
+++ whetherWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  whetherWeather
+//
+//  Created by Brett Bauman on 6/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

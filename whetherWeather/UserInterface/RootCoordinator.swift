7466b65242a7de7aa84e2b74002cf36595e00aa8
Brett Bauman <bauman.brett3@gmail.com>
2021-06-12 12:52:17 -0700

    detail view

--- whetherWeather/UserInterface/RootCoordinator.swift
+++ whetherWeather/UserInterface/RootCoordinator.swift
@@ -30,7 +30,7 @@ extension RootCoordinator: ForecastViewControllerDelegate {
         _ forecastViewController: ForecastViewController,
         didSelect forecast: Forecast
     ) {
-        
+        pushViewController(SingleDayForecastViewController(forecast: forecast), animated: true)
     }
 }
 
--- whetherWeather/UserInterface/Weather/SingleDayForecastViewController.swift
+++ whetherWeather/UserInterface/Weather/SingleDayForecastViewController.swift
@@ -6,3 +6,50 @@
 //
 
 import Foundation
+import UIKit
+
+class SingleDayForecastViewController: UIViewController {
+    
+    private lazy var descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.font = .preferredFont(forTextStyle: .body)
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    private let forecast: Forecast
+    
+    init(forecast: Forecast) {
+        self.forecast = forecast
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setUpView()
+        setUpLayout()
+    }
+    
+    private func setUpView() {
+        view.backgroundColor = .white
+        
+        descriptionLabel.text = forecast.description
+    }
+    
+    private func setUpLayout() {
+        let stackView = UIStackView(arrangedSubviews: [UIView(), descriptionLabel, UIView()])
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.distribution = .equalSpacing
+        stackView.directionalLayoutMargins = .init(top: 24, leading: 24, bottom: 24, trailing: 24)
+        stackView.isLayoutMarginsRelativeArrangement = true
+        
+        view.coverWithSubview(stackView)
+    }
+}
